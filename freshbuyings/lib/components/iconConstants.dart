3ec9e6a769503274089f27cbf15a6159b3c5957e
SanjaySiddharth <64358580+SanjaySiddharth@users.noreply.github.com>
2021-05-13 18:45:32 +0530

    nm

--- freshbuyings/lib/components/iconConstants.dart
+++ freshbuyings/lib/components/iconConstants.dart
@@ -1,3 +1,7 @@
+import 'package:flutter/material.dart';
+
 const berryicon = "assets/icons/berry1.svg";
+
 const strawberryIcon = "assets/icons/berry2.svg";
 const watermelonIcon = "assets/icons/watermelon.svg";
+const kPrimaryColor = Color(0xFFD8D8D8);
